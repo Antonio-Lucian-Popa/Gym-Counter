9bd3bc7aea436b7114efc09dd679e3f5b382cafc
AsuSoftware <asu.software.dev@gmail.com>
2023-10-03 16:50:06 +0300

    Initial Commit

---
+++ Gym Counter Watch App/Gym_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_CounterApp.swift
+//  Gym Counter Watch App
+//
+//  Created by Antonio Lucian on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Gym_Counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
